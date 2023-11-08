23f4ea06c15119554e4c74221c14794bbff56967
Pablo Rojas <pablorojasa@guanajuato.gob.mx>
2023-11-08 11:27:08 -0600

    proyecto de chistes y frases v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,61 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [joke, setJoke] = useState("");
+  const [chiste, setChiste] = useState("");
+  const [frase, setFrase] = useState("")
+  const [contenido, setContenido] = useState('chiste');
 
-  const getJoke = async () => {
-    // Obtenemos un chiste de la API
-    
-    const response = await fetch("https://icanhazdadjoke.com/", {
+  const getChiste = async () => {
+    const response = await fetch('https://icanhazdadjoke.com/', {
       headers: {
-        Accept: "application/json",
+        Accept: 'application/json',
       },
-    })
-    const joke = await response.json();
-    console.log(joke)
-    //const joke = await response.json();
+    });
 
-    // Actualizamos el estado con el chiste
-    setJoke(joke.joke);
+    try {
+      const chiste = await response.json();
+      setContenido('chiste');
+      setChiste(chiste.joke);
+    } catch (error) {
+      console.log(error);
+      setChiste('');
+    }
+  };
+
+  const getFrase = async () => {
+    const response = await fetch('https://quote-garden.onrender.com/api/v3/quotes/random');
+
+    try {
+      const frase = await response.json();
+      setContenido('frase');
+      setFrase(frase.data[0].quoteText);
+    } catch (error) {
+      console.log(error);
+      setFrase('');
+    }
+  };
+
+  useEffect(() => {
+    getChiste();
+  }, []);
+
+  const getButton = () => {
+    if (contenido === 'chiste') {
+      return <button onClick={getFrase}>Frases</button>;
+    } else {
+      return <button onClick={getChiste}>Chistes</button>;
+    }
   };
 
   return (
     <>
       <div>
-        <button onClick={getJoke}>Obtener chiste</button>
-        <p>{joke}</p>
+        {getButton()}
+        <p>{contenido === 'chiste' ? chiste : frase}</p>
       </div>
     </>
-  );
+  )
 }
 
 export default App
